b9ad5c7d1844ba082790a027af665da2fab546e7
reko8680 <$NsrEi24021987>
2022-12-18 13:37:38 +0100

    update version

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="mozilla")
-lat = 53.563218757860106
-long = 10.210500290046626
-coords = str(lat) + " " + str(long)
-location = geolocator.reverse(coords)
+location = geolocator.reverse("53.563218757860106, 10.210500290046626")
 print(location.address.split(","))
 print(location.address.split(",")[0].isnumeric() or location.address.split(",")[0][0].isdigit())
 print((location.latitude, location.longitude))
